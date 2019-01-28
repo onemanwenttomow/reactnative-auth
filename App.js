b202b20f85cff8d3794d25b9da04016cc600e96e
Peter Anderson <pete.anderson@gmail.com>
2019-01-28 16:11:11 +0100

    secrets added

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Constants } from 'expo';
 import * as firebase from 'firebase';
 import { Header, Button, Spinner, CardItem } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
@@ -13,14 +14,8 @@ export default class App extends React.Component {
     }
 
     componentWillMount() {
-        firebase.initializeApp({
-            apiKey: 'AIzaSyA7nPfCkuaiU_1mOLP0__pqioSlUno1I24',
-            authDomain: 'auth-16542.firebaseapp.com',
-            databaseURL: 'https://auth-16542.firebaseio.com',
-            projectId: 'auth-16542',
-            storageBucket: 'auth-16542.appspot.com',
-            messagingSenderId: '924791070380'
-        });
+        firebase.initializeApp(Constants.manifest.extra.firebaseConfig);
+        console.log(Constants.manifest.extra.firebaseConfig);
 
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
